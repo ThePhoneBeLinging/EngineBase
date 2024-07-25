ef550101fecba8981dd893a3e2d611321f4f9fe6
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-07-25 03:21:01 +0200

    Added auto memory allocation when adding out of bounds textures

--- TextureController.cpp
+++ TextureController.cpp
@@ -10,11 +10,20 @@
 TextureController::TextureController()
 {
     InitWindow(800,600,"M3");
-    this->mTextures.resize(5);
+    this->mTextures.resize(spacesPerResize);
 }
-
+//TODO Check this function, something seems off with the increments in size;
 void TextureController::addTexture(Texture2D texture, int firstIndex, int secondIndex)
 {
+    while (mTextures.capacity() <= firstIndex)
+    {
+        mTextures.resize(mTextures.capacity() + spacesPerResize);
+    }
+
+    while (mTextures[firstIndex].capacity() <= secondIndex)
+    {
+        mTextures[firstIndex].resize(mTextures.capacity() + spacesPerResize);
+    }
     this->mTextures[firstIndex][secondIndex] = texture;
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     std::shared_ptr<TextureController> textureController = std::make_shared<TextureController>();
-    textureController->addTexture(LoadTexture("../BBishop.png"),0,0);
+    textureController->addTexture(LoadTexture("../BBishop.png"),1000,0);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
