8c4691c3ed5eb8928bb55045ee2abcbf07644097
Elias Larsen <s235125@dtu.dk>
2024-08-19 23:47:06 +0200

    Reworked collisionchecker

--- src/Components/BaseFiles/ShapesPointChecker.cpp
+++ src/Components/BaseFiles/ShapesPointChecker.cpp
@@ -13,13 +13,28 @@ bool ShapesPointChecker::rectanglePointInsideChecker(Object *object, int x, int 
 
 bool ShapesPointChecker::rectangleCollisionChecker(DrawAbleObject *drawAbleObject, DrawAbleObject *otherObject)
 {
+
     int x1 = drawAbleObject->getX();
     int x2 = drawAbleObject->getX() + drawAbleObject->getWidth();
 
     int y1 = drawAbleObject->getY();
     int y2 = drawAbleObject->getY() + drawAbleObject->getHeight();
 
+    for (int i = drawAbleObject->getY(); i < y2; i++)
+    {
+        if (otherObject->isPointInside(x1, i) || otherObject->isPointInside(x2, i))
+        {
+            return true;
+        }
+    }
+    for (int k = drawAbleObject->getX(); k < x2; k++)
+    {
+        if (otherObject->isPointInside(k, y1) || otherObject->isPointInside(k, y2))
+        {
+            return true;
+        }
+    }
 
-    return otherObject->isPointInside(x1, y1) || otherObject->isPointInside(x2, y1) ||
-           otherObject->isPointInside(x1, y2) || otherObject->isPointInside(x2, y2);
+
+    return false;
 }
