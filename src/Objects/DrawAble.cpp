8e1ee70036a9441b0901068a87524ba3f5efe4bf
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-09-01 23:33:43 +0200

    Deleted old EngineBase and setup a sampleClient

---
+++ src/Objects/DrawAble.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elias Aggergaard Larsen on 01/09/2024.
+//
+
+#include "DrawAble.h"
---
+++ src/Objects/DrawAble.h
@@ -0,0 +1,14 @@
+//
+// Created by Elias Aggergaard Larsen on 01/09/2024.
+//
+
+#ifndef DRAWABLE_H
+#define DRAWABLE_H
+
+
+class DrawAble
+{
+};
+
+
+#endif //DRAWABLE_H
